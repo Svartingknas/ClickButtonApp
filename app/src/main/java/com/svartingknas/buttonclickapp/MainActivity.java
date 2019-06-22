5519bd729fdd891f309b07f41fa15ce52a067bc6
svartingknas <svartingknas@gmail.com>
2019-06-21 18:19:12 -0600

    update scrolling method

--- app/src/main/java/com/svartingknas/buttonclickapp/MainActivity.java
+++ app/src/main/java/com/svartingknas/buttonclickapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.svartingknas.buttonclickapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         button = (Button) findViewById(R.id.button);
         textView = (TextView) findViewById(R.id.textView);
         textView.setText(" ");
+        textView.setMovementMethod(new ScrollingMovementMethod());
         View.OnClickListener ourOnClickListener = new View.OnClickListener(){
             @Override
             public void onClick(View view){
